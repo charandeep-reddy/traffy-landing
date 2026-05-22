2ad86fdbb8c306e723c43e0d11cea7eb0c980f9c
Charandeep <sangaticherry3306@gmail.com>
2026-05-22 18:30:36 +0530

    Component breakdown, mobile fixes, hover animations, URL constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const APP_VERSION = '1.0.0';
+export const REPO = 'charandeep-reddy/traffy';
+export const REPO_URL = `https://github.com/${REPO}`;
+export const DOWNLOAD_URL = `${REPO_URL}/releases/download/v${APP_VERSION}/Traffy.dmg`;
+export const LICENSE_URL = `${REPO_URL}/blob/main/LICENSE`;
