98919a3f0bf4ba113363b043506d7675c3459b77
Charandeep <sangaticherry3306@gmail.com>
2026-05-25 16:05:40 +0530

    Bump version to 1.0.1

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.0.1';
 export const REPO = 'charandeep-reddy/traffy';
 export const REPO_URL = `https://github.com/${REPO}`;
 export const DOWNLOAD_URL = `${REPO_URL}/releases/download/v${APP_VERSION}/Traffy.dmg`;
